2ddcc98ac037db4061cf14b55947a9d129b7c75e
Mohd Sanabil <mohdsanabil4557@gmail.com>
2024-05-06 15:19:36 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import wikipedia
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+
+def index():
+    data = request.get_json()
+    input_data = data['queryResult']['queryText']
+    # print(data)
+    # print(input_data)
+    
+    output_data = wikipedia.summary(input_data, sentences=1)
+    # print(output_data)
+    response = {
+        'fulfillmentText': output_data
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=False)
